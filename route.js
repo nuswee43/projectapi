118b694c427688e6f49dc2a7ebf2b017f41406c9
minnyww <minrrverse@gmail.com>
2018-04-08 21:05:09 +0700

    Update API

--- route.js
+++ route.js
@@ -14,11 +14,26 @@ router.get('/getPatient', async (req, res) => {
     res.send(data);
 })
 
+router.get('/getMax', async (req, res) => {
+    var maxHN = await knex.table('Queue')
+        .select()
+        .where('roomId',1)
+        .max('queueId as maxQueueId')
+    console.log(maxHN[0].maxQueueId)
+    res.send('EIEI');
+})
+
+
 router.post('/addPatientQ', async (req, res) => {
-    var data = await knex.table('Queue')
-        // .insert({id:'101',textmessage:'yayayya'})
+    var maxHN = await knex.table('Queue')
+        .select()
+        .where('roomId',req.body.roomId)
+        .max('queueId as maxQueueId')
+    console.log(maxHN[0].maxQueueId)
+    res.send('EIEI');
+    await knex.table('Queue')
         .insert({
-            queueId: req.body.HN,
+            queueId: maxHN[0].maxQueueId+1,
             roomId: req.body.roomId,
             Date: req.body.Date,
             statusId: req.body.statusId,
@@ -27,31 +42,58 @@ router.post('/addPatientQ', async (req, res) => {
             forward: req.body.forward,
             nurseId: req.body.nurseId
         })
+    res.send('Success')
+})
 
-}),
+router.get('/getDepartment', async (req, res) => {
+    var data = await knex.table('Department')
+        .select()
+    res.send(data);
+})
+
+router.get('/getDepartment/:id', async (req, res) => {
+    var data = await knex.table('Department')
+        .select()
+        .where('departmentId', req.params.id)
+    res.send(data);
+})
+
+router.get('/getRoom/:id', async (req, res) => {
+    var data = await knex.table('Room')
+        .select()
+        .where('departmentId', req.params.id)
+    res.send(data);
+})
+
+router.get('/loginNurse/:id', async (req, res) => {
+    var data = await knex.table('Nurse')
+        .select()
+        .where('empId', req.params.id)
+    res.send(data);
+})
 
 
 
-    // router.put('/updatePatientQ', async (req, res) => {
-    //     var data = await knex.table('Queue')
-    //         .update({
-    //             textmessage: 'hahaaaa'
-    //         })
-    //         .where({
-    //             id: '101'
-    //         })
-    //     res.end('success')
+// router.put('/updatePatientQ', async (req, res) => {
+//     var data = await knex.table('Queue')
+//         .update({
+//             textmessage: 'hahaaaa'
+//         })
+//         .where({
+//             id: '101'
+//         })
+//     res.end('success')
 
-    // })
+// })
 
-    router.delete('/deletePatientQ', async (req, res) => {
-        var data = await knex.table('Queue')
-            .where({
-                HN: req.body.HN
-            })
-            .delete()
-        res.end('success')
-    })
+router.delete('/deletePatientQ', async (req, res) => {
+    var data = await knex.table('Queue')
+        .where({
+            HN: req.body.HN
+        })
+        .delete()
+    res.end('success')
+})
 
 router.post('/checkHN', async (req, res) => {
     var data = await knex.table('Patient')
@@ -76,17 +118,25 @@ router.post('/checkUsername', async (req, res) => {
 router.post('/getHN', async (req, res) => {
     var data = await knex.table('Patient')
         .select()
+        .where('HN', req.body.HN)
+    res.send(data);
+})
+
+router.post('/getRoom', async (req, res) => {
+    var data = await knex.table('Room')
+        .select()
         .where({ HN: req.body.HN })
     res.send(data);
 })
 
+
 router.get('/getQueue', async (req, res) => {
     var data = await knex.table('Queue')
-    .join('Room', 'Queue.roomId', '=', 'Room.roomId')
-    .join('Department','Room.departmentId','=','Department.departmentId')
-    .join('Patient','Queue.HN','=','Patient.HN')
-    .select()
-    
+        .join('Room', 'Queue.roomId', '=', 'Room.roomId')
+        .join('Department', 'Room.departmentId', '=', 'Department.departmentId')
+        .join('Patient', 'Queue.HN', '=', 'Patient.HN')
+        .select()
+
     res.send(data);
 })
 
